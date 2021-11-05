7eb1ee81c26898c57a32cdee465d5ac668acef33
ollykr <okremianski@gmail.com>
2021-11-05 10:11:26 -0400

    Add Lesson 7

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-console.log("hello, vue");
+// Create Vue App
+// { root component }
+const app = Vue.createApp({
+	// any data, functions, component template to run inside #app
+	// template: "<h2>I am the template</h2>",
+});
+
+// Mount Vue App into a DOM (represented by "div id="app" in index.html)
+app.mount("#app");
