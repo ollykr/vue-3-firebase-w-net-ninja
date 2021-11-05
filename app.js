fd456c811d0b7133ae4d6d884ad59d79e29b5124
ollykr <okremianski@gmail.com>
2021-11-05 18:49:19 -0400

    Add lesson-11 - other mouse events

--- app.js
+++ app.js
@@ -12,6 +12,8 @@ const app = Vue.createApp({
 			title: "The Final Empire",
 			author: "Brandon Sanderson",
 			age: 45,
+			x: 0, // 0 by default
+			y: 0, // 0 by default
 		};
 	},
 	// Create a component method
@@ -19,6 +21,20 @@ const app = Vue.createApp({
 		toggleShowBooks() {
 			// Toggle functionality - hide/show
 			this.showBooks = !this.showBooks;
+		},
+		// When we use events like mouseover, mouseleave, etc, we automatically have access to an optional object - event, or 'e' (or whatever we want to call it) which we have to use as a first parameter in a function, 'data' (it can be called anything) is a second parameter which is used for our custom '5' argument
+		handleEvents(e, data) {
+			console.log(e, e.type); // I can see an object properties in the console
+			// e.type lets me to see in console event's type, e.g mouseover, mouseleave, etc
+			// If 'data' has an argument (it does - 5), then add it to console
+			if (data) {
+				console.log(data);
+			}
+		},
+		handleMousemove(e) {
+			// update x/y properties
+			this.x = e.offsetX;
+			this.y = e.offsetY;
 		},
 	},
 });
