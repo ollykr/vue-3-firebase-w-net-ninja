c96fb77e6a66b767ac1b64fd56db3e503c1adff6
ollykr <okremianski@gmail.com>
2021-11-05 11:01:40 -0400

    Lesson 8

--- app.js
+++ app.js
@@ -3,6 +3,16 @@
 const app = Vue.createApp({
 	// any data, functions, component template to run inside #app
 	// template: "<h2>I am the template</h2>",
+	// data function that serves as a some sort of Vue variable used inside #app in index.html
+	data() {
+		// Return an object
+		return {
+			// properties
+			title: "The Final Empire",
+			author: "Brandon Sanderson",
+			age: 45,
+		};
+	},
 });
 
 // Mount Vue App into a DOM (represented by "div id="app" in index.html)
