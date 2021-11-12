18c5cb14b879dc76dcd6d3c87c6a87eed19bb7ec
ollykr <okremianski@gmail.com>
2021-11-12 17:52:28 -0500

    Add global styles, update scoped Modal styles

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -3,11 +3,15 @@
 	<!-- A semitransparent backdrop behind a modal -->
 	<div class="backdrop">
 		<div class="modal">
+			<h1>Modal Title</h1>
 			<p>modal content</p>
 		</div>
 	</div>
 </template>
-
+<!-- scoped to this Modal component -->
+<!-- Vue scops it by adding random data-attributes to Modal styles' classes in the DOM, there is a slight performance hit with this approach as Vue has to inject it every time page loads-->
+<!-- <style scoped> -->
+<!-- Better way just to make CSS class more specific .modal h1 {} -->
 <style>
 .modal {
 	width: 400px;
@@ -22,5 +26,14 @@
 	background: rgba(0, 0, 0, 0.5);
 	width: 100%;
 	height: 100%;
+}
+.modal h1 {
+	color: #03cfb4;
+	border: none;
+	padding: 0;
+}
+/* Overriding global styles for this component only */
+.modal p {
+	font-style: normal;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -6,6 +6,8 @@ import { createApp } from "vue";
 // This is a Vue component
 // We import it from a Vue file (aka Single File Component) - App.vue
 import App from "./App.vue";
+// import global styles
+import "./assets/global.css";
 // Inside we pass in "App" object
 // App is a root component that then mounts to the DOM to "#app" location
 createApp(App).mount("#app");
