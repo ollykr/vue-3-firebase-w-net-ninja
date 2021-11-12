3326e0b7c3f6f7333b9153749bc034f5d9a93998
ollykr <okremianski@gmail.com>
2021-11-12 14:46:26 -0500

    Add template refs example in App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,6 @@
+<!-- Notes about Lesson 22 -->
+<!-- Template refs allow us to store a reference to a DOM element inside a variable, once we have that refs, we can use regular JS methods and properties, e.g change classes, styles, text content, etc  -->
+
 <!-- Each Component has template, script and style in every Vue project -->
 <!-- However, script and style are optional, but Template is mandatory -->
 <!-- When we render a Component to DOM, its template is injected into the DOM  -->
@@ -7,6 +10,10 @@
 <template>
 	<!-- From data() property -->
 	<h1>{{ title }}</h1>
+	<!-- Template refs example -->
+	<!-- If we want to get a handle of the input element, we need to use "ref='name'" -->
+	<input type="text" ref="name" />
+	<button @click="handleClick">click me</button>
 </template>
 
 <script>
@@ -18,6 +25,18 @@ export default {
 		return {
 			title: "My First Vue App :)",
 		};
+	},
+	methods: {
+		handleClick() {
+			// 'this' refers to this component
+			// via $refs object I can access any ref="" in the <template> above, e.g ref="name"
+			console.log(this.$refs.name);
+			// We can add/remove CSS class using template refs
+			// Regular JS, no Vue stuff
+			this.$refs.name.classList.add("active");
+			// make text field or input element on focus after clicking it
+			this.$refs.name.focus();
+		},
 	},
 	// 'components' property registers any nested components (e.g "HelloWorld")
 	// components: {
