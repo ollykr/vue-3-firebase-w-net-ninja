8ada3dcc623040cbd63af6eec2e7052776db1d23
ollykr <okremianski@gmail.com>
2021-11-12 17:08:58 -0500

    Create Modal component and add it to App

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,4 @@
-<!-- Notes about Lesson 22 -->
+<!-- Notes about Lesson 23 -->
 <!-- Template refs allow us to store a reference to a DOM element inside a variable, once we have that refs, we can use regular JS methods and properties, e.g change classes, styles, text content, etc  -->
 
 <!-- Each Component has template, script and style in every Vue project -->
@@ -7,41 +7,34 @@
 <!-- A root tag has always be <template> which is a Vue tag, not HTML tag -->
 <!-- But inside <template> you can organize your code however you wish -->
 <!-- Vue 3 allows multiple root elements inside <template> -->
+<!-- If we wanted to, we could create an entire webpage inside <template> component below, but it will quickly become too large, gigantic. So it is not good idea to place everything in one single component, in one single vue file -->
+<!-- Instead we should split our Vue code into multiple components: navbar, header, sidebar, article, footer components -->
+<!-- Components structure (or a "component tree") can be: App.vue (i.e root component): Header.vue, Article.vue (it has "child" components: Content.vue and Comments.vue to which "Article.vue" is a "parent" component), Footer.vue  -->
 <template>
 	<!-- From data() property -->
 	<h1>{{ title }}</h1>
-	<!-- Template refs example -->
-	<!-- If we want to get a handle of the input element, we need to use "ref='name'" -->
-	<input type="text" ref="name" />
-	<button @click="handleClick">click me</button>
+	<!-- Use Modal property that was imported from Modal.vue and then registered as a Modal component inside of App component via <script> -->
+	<!-- Naming conventions : start your single vue file with a capital letter, if single file vue component has the same filename as regular html file, it may cause conflict, therefore Modal.vue, not modal.vue -->
+	<Modal />
 </template>
 
 <script>
+// 'Modal' here can be called whatever we want
+// Also we don't have to add '.vue'
+import Modal from "./components/Modal.vue";
+
 // Objects "default" being exported
 // This object represents the root component
 export default {
 	name: "App",
+	// We register Modal component inside of App component via 'components' object
+	components: { Modal },
+	// Our component object
 	data() {
 		return {
 			title: "My First Vue App :)",
 		};
 	},
-	methods: {
-		handleClick() {
-			// 'this' refers to this component
-			// via $refs object I can access any ref="" in the <template> above, e.g ref="name"
-			console.log(this.$refs.name);
-			// We can add/remove CSS class using template refs
-			// Regular JS, no Vue stuff
-			this.$refs.name.classList.add("active");
-			// make text field or input element on focus after clicking it
-			this.$refs.name.focus();
-		},
-	},
-	// 'components' property registers any nested components (e.g "HelloWorld")
-	// components: {
-	// 	HelloWorld,
-	// },
 };
 </script>
 <!-- Global Styles -->
