693c6cfe4a63595bd56a941425e5bbb3c28ff6b6
ollykr <okremianski@gmail.com>
2021-11-12 13:36:14 -0500

    Update App.vue with more notes or comments

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,34 @@
+<!-- Each Component has template, script and style in every Vue project -->
+<!-- However, script and style are optional, but Template is mandatory -->
+<!-- When we render a Component to DOM, its template is injected into the DOM  -->
+<!-- A root tag has always be <template> which is a Vue tag, not HTML tag -->
+<!-- But inside <template> you can organize your code however you wish -->
+<!-- Vue 3 allows multiple root elements inside <template> -->
 <template>
-<!-- Template exists in every Vue project -->
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+	<!-- First root element -->
+	<img alt="Vue logo" src="./assets/logo.png" />
+	<!-- Second root element -->
+	<HelloWorld msg="Welcome to Your Vue.js App" />
 </template>
 
 <script>
-// Script exists in every Vue project
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
 // Objects being exported
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+	name: "App",
+	components: {
+		HelloWorld,
+	},
+};
 </script>
 
 <style>
-/* Style exists in every Vue project */
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+	font-family: Avenir, Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+	margin-top: 60px;
 }
 </style>
